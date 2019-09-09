5cd150e197542c344c0373892dc457c661cc0d49
yang <exvincse@gmail.com>
2019-09-09 15:28:11 +0800

    修正年曆

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,3 @@
 module.exports = {
   publicPath: './',
-  // npm i http-proxy-middleware --save
-  //   devServer: {
-  //     https: true,
-  //     proxy: {
-  //       '/jsondata': {
-  //         target: 'https://challenge.thef2e.com/api/thef2e2019/stage6/',
-  //         changeOrigin: true,
-  //         pathRewrite: {
-  //           '^/api': '',
-  //         },
-  //       },
-  //     },
-  //   },
 };
